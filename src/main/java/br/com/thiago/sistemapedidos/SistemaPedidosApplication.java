87cdcbd78f9f812382f164fded877097bca044b8
Thiago Martins <devthiagomartins@gmail.com>
2021-12-13 10:08:30 -0300

    Projeto criado

---
+++ src/main/java/br/com/thiago/sistemapedidos/SistemaPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.sistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidosApplication.class, args);
+	}
+
+}
