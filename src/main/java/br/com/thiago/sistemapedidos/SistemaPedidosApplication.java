303d67aca83ca5d5d185b46743ab916945547ef0
Thiago Martins <devthiagomartins@gmail.com>
2021-12-14 09:37:27 -0300

    criando operacao de instanciacao commandLineRunner

--- src/main/java/br/com/thiago/sistemapedidos/SistemaPedidosApplication.java
+++ src/main/java/br/com/thiago/sistemapedidos/SistemaPedidosApplication.java
@@ -1,13 +1,31 @@
 package br.com.thiago.sistemapedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.thiago.sistemapedidos.domain.Categoria;
+import br.com.thiago.sistemapedidos.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class SistemaPedidosApplication {
+public class SistemaPedidosApplication implements CommandLineRunner{
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
