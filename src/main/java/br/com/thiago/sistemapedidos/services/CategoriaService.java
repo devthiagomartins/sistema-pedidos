5ddf3951009d257fbd8f790ff4d66b02812ad039
Thiago Martins <devthiagomartins@gmail.com>
2021-12-15 14:01:28 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/br/com/thiago/sistemapedidos/services/CategoriaService.java
+++ src/main/java/br/com/thiago/sistemapedidos/services/CategoriaService.java
@@ -2,9 +2,14 @@ package br.com.thiago.sistemapedidos.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
 import br.com.thiago.sistemapedidos.domain.Categoria;
 import br.com.thiago.sistemapedidos.repositories.CategoriaRepository;
 import br.com.thiago.sistemapedidos.services.exceptions.ObjectNotFoundException;
@@ -57,5 +62,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
