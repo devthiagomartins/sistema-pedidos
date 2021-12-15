1445de03d07035ed59c9348b7dc3e28d07ef18b3
Thiago Martins <devthiagomartins@gmail.com>
2021-12-15 12:03:22 -0300

    inserindo nova Categoria com POST

--- src/main/java/br/com/thiago/sistemapedidos/resources/CategoriaResource.java
+++ src/main/java/br/com/thiago/sistemapedidos/resources/CategoriaResource.java
@@ -1,12 +1,17 @@
 package br.com.thiago.sistemapedidos.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import br.com.thiago.sistemapedidos.domain.Categoria;
 import br.com.thiago.sistemapedidos.services.CategoriaService;
 
@@ -22,6 +27,17 @@ public class CategoriaResource {
 		
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 	
 }
