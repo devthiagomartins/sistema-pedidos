55301e5b4a831ea7cd1b81efd8055f280e8de074
Thiago Martins <devthiagomartins@gmail.com>
2021-12-17 10:32:04 -0300

    validacao customizada: email nao repetido no Update Cliente

--- src/main/java/br/com/thiago/sistemapedidos/dto/ClienteDTO.java
+++ src/main/java/br/com/thiago/sistemapedidos/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.thiago.sistemapedidos.domain.Cliente;
+import br.com.thiago.sistemapedidos.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
